a45d6c0d2cc17ce866167cabd764cdaff8c38fbc
yahyaelkaed <129843158+yahyaelkaed@users.noreply.github.com>
2026-09-05 00:02:49 +0100

    Set Gemini_Generated_Image_nqaw0jnqaw0jnqaw as primary cover image for Jar Opener

--- src/data/products.ts
+++ src/data/products.ts
@@ -160,10 +160,9 @@ export const kitchenProducts: Product[] = [
       { label: "Color Options", value: "Clean Modern White" }
     ],
     images: [
-      "/images/products/jar-opener-weak-hands/main-product.jpg",
+      "/images/products/jar-opener-weak-hands/Gemini_Generated_Image_nqaw0jnqaw0jnqaw.jfif",
       "/images/products/jar-opener-weak-hands/Gemini_Generated_Image_t9uwhvt9uwhvt9uw.jfif",
-      "/images/products/jar-opener-weak-hands/Gemini_Generated_Image_2udojb2udojb2udo.jfif",
-      "/images/products/jar-opener-weak-hands/Gemini_Generated_Image_nqaw0jnqaw0jnqaw.jfif"
+      "/images/products/jar-opener-weak-hands/Gemini_Generated_Image_2udojb2udojb2udo.jfif"
     ],
     isTop: false
   },
